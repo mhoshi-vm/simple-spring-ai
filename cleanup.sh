f2bb9b0819ab6979cfedbc649b5d98ac4bf4977f
mhoshi-vm <machi.hoshino@broadcom.com>
2025-10-08 15:30:07 +0900

    add cleanup.sh

---
+++ cleanup.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+set -x
+cf target -o system -s explore
+
+cf services | egrep -vw "azure-openai-gpt4.1-nano|my-postgres" | sed -e '1,3d' | awk '{print"cf delete-service -f "$1}' | bash -x
+cf service-keys azure-openai-gpt4.1-nano | sed '1,3d' | awk '{print"cf delete-service-key azure-openai-gpt4.1-nano -f "$1}' | bash -x
+cf service-keys my-postgres | sed '1,3d' | awk '{print"cf delete-service-key my-postgres -f "$1}' | bash -x
+cf apps | egrep -vw "prepush" | sed '1,3d' | awk '{print"cf delete -r -f "$1}' | bash -x
+
+find ~/Downloads -name "demo*" -maxdepth 1 -exec rm -rf {} \;
+find ~/Library/Application\ Support/JetBrains/ -name "*.http" -exec rm {} \;
+
+curl https://prepush.app.tas.vpsmart.aws.lespaulstudioplus.info/clear
